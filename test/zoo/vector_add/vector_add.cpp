bedc87d2c3b8fdcbdb0dd3f46fc66214d7c3d141
Phosphagos <phosphagos@outlook.com>
2025-07-25 16:52:36 +0800

    feat(zoo): add unittest of vector_add.

---
+++ test/zoo/vector_add/vector_add.cpp
@@ -0,0 +1,22 @@
+#include "vector_add.hpp"
+#include <cstdio>
+
+using namespace moke;
+
+static int TestSuitIndex = 0;
+
+template <class T>
+void ExecuteVectorAdd(size_t length) {
+    println("========= Test Suit {} =========", TestSuitIndex++);
+    println("Basic Info:");
+    println("    data type: {}", type_of<T>());
+    println("       length: {}", length);
+    VectorAddExecutor<T>{length}.execute();
+}
+
+int main() {
+    ExecuteVectorAdd<float>(1 << 20);
+    ExecuteVectorAdd<float>(64 << 20);
+    ExecuteVectorAdd<int32_t>(1 << 20);
+    ExecuteVectorAdd<int32_t>(64 << 20);
+}
