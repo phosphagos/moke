00fd2a86aba328c383efa2685804ed1ba32aa40c
phosphagos <phosphagos@outlook.com>
2026-05-13 22:05:24 +0800

    test(core): add hip support of matmul performance benchmark.

--- cmake/hip.cmake
+++ cmake/hip.cmake
@@ -4,3 +4,8 @@ enable_language(HIP)
 set(CMAKE_HIP_STANDARD 20)
 set(CMAKE_HIP_STANDARD_REQUIRED ON)
 set(CMAKE_HIP_EXTENSIONS OFF)
+
+foreach(MOKE_HIP_INCLUDE_DIR IN LISTS hip_INCLUDE_DIRS)
+    # for clangd to detect hip includes
+    string(APPEND CMAKE_HIP_FLAGS " -isystem ${MOKE_HIP_INCLUDE_DIR}")
+endforeach()
