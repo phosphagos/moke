50669ae1c535ee2fbe71eb34c736eeca86c718c1
zhangyi <yizhang@birentech.com>
2026-06-04 15:13:13 +0800

    feat(core): add compile-time constant value K<N>.

--- moke/include/moke/meta/constant.hpp
+++ moke/include/moke/meta/constant.hpp
@@ -2,11 +2,16 @@
 #include <type_traits>
 
 namespace moke {
-// compile-time constant
+// compile-time constant type
 template <auto N>
 struct C : std::integral_constant<std::remove_cvref_t<decltype(N)>, N> {};
 
-template <auto N> using constant = C<N>;
+// compile-time constant value
+template <auto N> constexpr C<N> K{};
+
+// alias constant<N> to C<N>
+template <auto N>
+using constant = C<N>;
 
 // compile-time constant type for bool
 using true_t = C<true>;
