90c86f71cd4bea9902fa3debb1161960d393ad68
zhangyi <yizhang@birentech.com>
2026-06-18 18:33:40 +0800

    feat(core): add literal suffix operator of integral constant.

--- moke/include/moke/meta/constant.hpp
+++ moke/include/moke/meta/constant.hpp
@@ -1,13 +1,11 @@
 #pragma once
+#include "moke/common.hpp"
 #include <type_traits>
 
 namespace moke {
 // compile-time constant type
 template <auto N>
 struct C : std::integral_constant<std::remove_cvref_t<decltype(N)>, N> {};
-
-// compile-time constant value
-template <auto N> constexpr C<N> K{};
 
 // alias constant<N> to C<N>
 template <auto N>
@@ -20,4 +18,43 @@ using false_t = C<false>;
 // compile-time constant value for bool
 constexpr auto TRUE = C<true>{};
 constexpr auto FALSE = C<false>{};
+
+namespace meta {
+    template <size_t value, char digit>
+    MOKE_CONSTEVAL std::integral auto parse_integer_digit() noexcept {
+        static_assert(digit >= '0' && digit <= '9' || digit == '\'');
+        if constexpr (digit == '\'') {
+            return value;
+        } else {
+            return (value * 10) + (digit - '0');
+        }
+    }
+
+    template <size_t value>
+    MOKE_CONSTEVAL auto make_integral_constant() noexcept {
+        static_assert(value <= LONG_LONG_MAX);
+        if constexpr (value <= INT_MAX) {
+            return C<int(value)>{};
+        } else if constexpr(value <= LONG_MAX) {
+            return C<long(value)>{};
+        } else {
+            return C<(long long)(value)>{};
+        }
+    }
+
+    template <size_t value, char digit, char... digits>
+    MOKE_CONSTEVAL auto parse_integral_constant() noexcept {
+        constexpr size_t next_value = parse_integer_digit<value, digit>();
+        if constexpr (sizeof...(digits) == 0) {
+            return make_integral_constant<next_value>();
+        } else {
+            return parse_integral_constant<next_value, digits...>();
+        }
+    }
+} // namespace meta
 } // namespace moke
+
+namespace moke::literals {
+template <char... digits>
+MOKE_CONSTEVAL auto operator""_ic() noexcept { return meta::parse_integral_constant<0, digits...>(); }
+} // namespace moke::literals
--- test/unit_test/core/test_meta.cpp
+++ test/unit_test/core/test_meta.cpp
@@ -8,18 +8,23 @@ inline bool test_constant(moke::false_t) { return false; }
 
 TEST(TestMeta, TestConstant) {
     constexpr int v0 = C<128>{};
-    static_assert(v0 == 128);
-
     auto v1 = C<256>{};
+
+    static_assert(v0 == 128);
     static_assert(v1 == 256);
 
     constexpr auto b0 = TRUE;
     constexpr auto b1 = FALSE;
+    static_assert(std::same_as<decltype(b0), const true_t>);
+    static_assert(std::same_as<decltype(b1), const false_t>);
     static_assert(b0 == true);
     static_assert(b1 == false);
-
     EXPECT_TRUE(test_constant(TRUE));
     EXPECT_FALSE(test_constant(FALSE));
+
+    using namespace moke::literals;
+    static_assert(128_ic == C<128>{});
+    static_assert(1'024_ic == C<1024>{});
 }
 
 TEST(TestMeta, TestConstantTuple) {
